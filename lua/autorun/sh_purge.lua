e32ae98bff343e53bcd9624b9e38a0db67149da7
unknown <50420057+SirPlancake@users.noreply.github.com>
2026-04-11 19:31:29 +0100

    Add new loader for more details

--- lua/autorun/sh_purge.lua
+++ lua/autorun/sh_purge.lua
@@ -1,13 +1,11 @@
 SPC_PURGE = SPC_PURGE or {}
+SPC_PURGE.Version = "1.0.0"
 SPC_PURGE.Settings = SPC_PURGE.Settings or {}
 SPC_PURGE.Nets = SPC_PURGE.Nets or {}
 SPC_PURGE.Languages = SPC_PURGE.Languages or {}
 SPC_PURGE.InternalData = SPC_PURGE.InternalData or {}
 SPC_PURGE.States = SPC_PURGE.States or {
-	IDLE = 0,
-	PREPARE = 1,
-	ACTIVE = 2,
-	GRACE = 3
+	IDLE = 0, PREPARE = 1, ACTIVE = 2, GRACE = 3
 }
 
 function SPC_PURGE.Print(...)
@@ -19,11 +17,56 @@ function SPC_PURGE.Print(...)
 	) Msg("\n")
 end
 
+local function LoadDirectory(Path)
+	local Files, Folders = file.Find(Path .. "/*", "LUA")
+
+	for _, FileName in ipairs(Files) do
+		local FilePath = Path .. "/" .. FileName
+
+		if (CLIENT) then
+			include(FilePath)
+		else
+			if (FileName:StartWith("cl_")) then
+				AddCSLuaFile(FilePath)
+				SPC_PURGE.Print("Loaded CL: " .. FilePath)
+			elseif (FileName:StartWith("sh_")) then
+				AddCSLuaFile(FilePath) include(FilePath)
+				SPC_PURGE.Print("Loaded SH: " .. FilePath)
+			else
+				include(FilePath)
+				SPC_PURGE.Print("Loaded SV: " .. FilePath)
+			end
+		end
+	end
+
+	return Files, Folders
+end
+
+local function LoadDirectoryRecursive(BasePath)
+	local _, Folders = LoadDirectory(BasePath)
+	for _, FolderName in ipairs(Folders) do
+		LoadDirectoryRecursive(BasePath .. "/" .. FolderName)
+	end
+end
+
 local function LoadAddon()
 	SPC_PURGE.Print("Loading...")
-	PIXEL.LoadDirectoryRecursive("spc_purge")
+	LoadDirectoryRecursive("spc_purge")
 	SPC_PURGE.Print("Loaded!")
 end
+
+hook.Add("Think", "SPC_PURGE.CheckForUpdate", function()
+	hook.Remove("Think", "SPC_PURGE.CheckForUpdate")
+	http.Fetch("https://raw.githubusercontent.com/SirPlancake/SPC_PURGE/main/VERSION", function(Body)
+		if (SPC_PURGE.Version ~= string.Trim(Body)) then
+			SPC_PURGE.Print("------------------------------------------------------------------")
+			SPC_PURGE.Print(string.format("There is an update available. (%s -> %s)", SPC_PURGE.Version, Body))
+			SPC_PURGE.Print("You can download it here: https://github.com/SirPlancake/SPC_PURGE")
+			SPC_PURGE.Print("------------------------------------------------------------------")
+			return
+		end
+	end)
+end)
 
 if (PIXEL and PIXEL.UI) then LoadAddon() return end
 hook.Add("PIXEL.UI.FullyLoaded", "SPC_PURGE.IsFrameworkLoaded", LoadAddon)
--- lua/spc_purge/core/sv_nets.lua
+++ lua/spc_purge/core/sv_nets.lua
@@ -1,4 +1,3 @@
 for _, String in pairs(SPC_PURGE.Nets) do
     util.AddNetworkString(String)
-    SPC_PURGE.Print("Registered NWString => " .. String)
 end
